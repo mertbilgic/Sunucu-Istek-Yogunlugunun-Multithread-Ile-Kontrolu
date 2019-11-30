c9aaaa97696425a6c53937d748c8e94e467e2cc2
mertbilgic <34304850+mertbilgic@users.noreply.github.com>
2019-11-30 12:49:49 +0300

    property added to requestdata class

--- src/RequestData.java
+++ src/RequestData.java
@@ -1,13 +1,44 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//Bu sınıfı listelerimize verilerek eklerken gerekli objeleri oluşturmak için kullanıyoruz.
 
-/**
- *
- * @author Mert Bilgic
- */
 public class RequestData {
-    
+
+    private String clientIP;
+    private String request;
+    private String response;
+
+    public RequestData(String clientIP, String request, String response) {
+        this.clientIP = clientIP;
+        this.request = request;
+        this.response = response;
+    }
+
+    public RequestData(String clientIP, String request) {
+        this.clientIP = clientIP;
+        this.request = request;
+    }
+
+    public String getClientIP() {
+        return clientIP;
+    }
+
+    public void setClientIP(String clientIP) {
+        this.clientIP = clientIP;
+    }
+
+    public String getRequest() {
+        return request;
+    }
+
+    public void setRequest(String request) {
+        this.request = request;
+    }
+
+    public String getResponse() {
+        return response;
+    }
+
+    public void setResponse(String response) {
+        this.response = response;
+    }
+
 }
