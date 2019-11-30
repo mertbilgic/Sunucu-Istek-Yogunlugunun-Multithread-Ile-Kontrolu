2858289c66a3e8bce672ba4203d417f3fa01ec46
mertbilgic <34304850+mertbilgic@users.noreply.github.com>
2019-11-30 13:03:46 +0300

    request method add subserver

--- src/SubServer.java
+++ src/SubServer.java
@@ -1,13 +1,53 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author Mert Bilgic
- */
-public class SubServer {
-    
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class SubServer extends Server {
+
+    //Main server için bir obje oluşturuyoruz.
+    //Server server = Main.server.get(MainServerIndex);
+
+    public SubServer(String serverName, int capacity, int requestTime, int responseTime, int serverIP, boolean sleep, int maxRequestCount) {
+        super(serverName, capacity, requestTime, responseTime, serverIP, sleep, maxRequestCount);
+    }
+
+    //
+    public ArrayList<RequestData> request(ArrayList<RequestData> request, int serverIndex, int requestCount) {
+        synchronized (lock) {
+            //System.out.println(getServerName());
+            ArrayList<RequestData> temp = null;
+            requestCount = requestCountControl(requestData.size(), requestCount);
+            try {
+                Thread.sleep(getRequestTime());
+
+                //System.out.println(requestCount + "      " + mainServer.requestData.size());
+                //Bu işlemleri wait metodu ile yönetmem gerekiyor.
+                /*if (getRequestData().size() + requestCount < getCapacity()) {
+                    
+                    //Main.server.get(serverIndex);
+                    temp = new ArrayList<RequestData>(request.subList(startIndex, requestCount));
+
+                    requestData.addAll(temp);
+
+                } else {
+                    requestCount = getCapacity() - getRequestData().size();
+                    temp = new ArrayList<RequestData>(request.subList(startIndex, requestCount));
+
+                    requestData.addAll(temp);
+                }*/
+                //System.out.println("**************************************************************requestCount"+requestCount);
+                for (int i = 0; i < requestCount; i++) {
+                    requestData.add(new RequestData("2", "2"));
+
+                }
+                // temp = new ArrayList<RequestData>(request.subList(startIndex, requestCount));
+
+                //requestData.addAll(temp);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(SubServer.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            return temp;
+        }
+    }
 }
