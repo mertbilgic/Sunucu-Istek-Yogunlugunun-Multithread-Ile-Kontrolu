a57deb99d7309bd6e92450705a8de5531b84f42e
mertbilgic <34304850+mertbilgic@users.noreply.github.com>
2019-11-30 15:59:19 +0300

    response method add subserver

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author Mert Bilgic
- */
+import java.util.ArrayList;
+
 public class Main {
-    
+
+    //Static oluşturmamızın sebebi her yerden erişilebilir hale gelmesini sağlamak
+    static ArrayList<Server> server = new ArrayList<Server>();
+
+    public static void main(String[] args) {
+
+        System.out.println("//////////////// Main Thread Başladı");
+
+        //ThreadManeger sınıfındaki fonksiyonları kullanıp server yönetim işini
+        //yapmak için oluşturduğumu obje
+        ThreadManager tm = new ThreadManager();
+
+        server.add(new MainServer("MainServer", 10000, 200, 2500, 1, false, 1000, 700));
+        server.add(new SubServer("SubServer-1", 5000, 300, 3000, 2, false, 150));
+        server.add(new SubServer("SubServer-2", 5000, 300, 3000, 3, false, 150));
+
+    }
 }
--- src/SubServer.java
+++ src/SubServer.java
@@ -5,11 +5,14 @@ import java.util.logging.Logger;
 
 public class SubServer extends Server {
 
+    //Main server için bir obje oluşturuyoruz.
+    Server server = Main.server.get(MainServerIndex);
+
     public SubServer(String serverName, int capacity, int requestTime, int responseTime, int serverIP, boolean sleep, int maxRequestCount) {
         super(serverName, capacity, requestTime, responseTime, serverIP, sleep, maxRequestCount);
     }
 
-    //
+    //Balance metodu tarafından yönllendirilen requestler ilgili sunucuya burda ekleniyor.
     public ArrayList<RequestData> request(ArrayList<RequestData> request, int serverIndex, int requestCount) {
         synchronized (lock) {
             //System.out.println(getServerName());
@@ -18,15 +21,47 @@ public class SubServer extends Server {
             try {
                 Thread.sleep(getRequestTime());
 
-                for (int i = 0; i < requestCount; i++) {
+                temp = new ArrayList<RequestData>(request.subList(startIndex, requestCount));
+
+                requestData.addAll(temp);
+                /*for (int i = 0; i < requestCount; i++) {
                     requestData.add(new RequestData("2", "2"));
 
-                }
-
+                }*/
             } catch (InterruptedException ex) {
                 Logger.getLogger(SubServer.class.getName()).log(Level.SEVERE, null, ex);
             }
             return temp;
+        }
+    }
+
+    public void response() {
+
+        while (true) {
+            try {
+                //Bölme işlemi sırasında response işlemini sleep yapıyoruz
+                while (isSleep() == true) {
+                    Thread.sleep(100);
+                }
+
+                Thread.sleep(getResponseTime());
+
+                int requestCount = random.nextInt(getMaxRequestCount());
+                if (requestCount > requestData.size()) {
+
+                    Thread.sleep(100);
+                    requestCount = (requestData.size());
+                }
+
+                synchronized (lock) {
+                    ArrayList<RequestData> temp;
+                    temp = new ArrayList<RequestData>(requestData.subList(startIndex, requestCount));
+                    requestData.removeAll(temp);
+                    server.responseData.addAll(temp);
+                }
+            } catch (InterruptedException ex) {
+                Logger.getLogger(SubServer.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
     }
 
