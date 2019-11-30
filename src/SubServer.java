da2f4094549e9d71333e6d92b1e928269a5894be
mertbilgic <34304850+mertbilgic@users.noreply.github.com>
2019-11-30 13:06:33 +0300

    update subserver request

--- src/SubServer.java
+++ src/SubServer.java
@@ -5,9 +5,6 @@ import java.util.logging.Logger;
 
 public class SubServer extends Server {
 
-    //Main server için bir obje oluşturuyoruz.
-    //Server server = Main.server.get(MainServerIndex);
-
     public SubServer(String serverName, int capacity, int requestTime, int responseTime, int serverIP, boolean sleep, int maxRequestCount) {
         super(serverName, capacity, requestTime, responseTime, serverIP, sleep, maxRequestCount);
     }
@@ -21,33 +18,16 @@ public class SubServer extends Server {
             try {
                 Thread.sleep(getRequestTime());
 
-                //System.out.println(requestCount + "      " + mainServer.requestData.size());
-                //Bu işlemleri wait metodu ile yönetmem gerekiyor.
-                /*if (getRequestData().size() + requestCount < getCapacity()) {
-                    
-                    //Main.server.get(serverIndex);
-                    temp = new ArrayList<RequestData>(request.subList(startIndex, requestCount));
-
-                    requestData.addAll(temp);
-
-                } else {
-                    requestCount = getCapacity() - getRequestData().size();
-                    temp = new ArrayList<RequestData>(request.subList(startIndex, requestCount));
-
-                    requestData.addAll(temp);
-                }*/
-                //System.out.println("**************************************************************requestCount"+requestCount);
                 for (int i = 0; i < requestCount; i++) {
                     requestData.add(new RequestData("2", "2"));
 
                 }
-                // temp = new ArrayList<RequestData>(request.subList(startIndex, requestCount));
 
-                //requestData.addAll(temp);
             } catch (InterruptedException ex) {
                 Logger.getLogger(SubServer.class.getName()).log(Level.SEVERE, null, ex);
             }
             return temp;
         }
     }
+
 }
