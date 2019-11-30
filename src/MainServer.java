0dea03de0b24d0f9ac4a5da0ebd8dd41debf7f2f
mertbilgic <34304850+mertbilgic@users.noreply.github.com>
2019-11-30 15:44:23 +0300

    request add Mainserver

--- src/MainServer.java
+++ src/MainServer.java
@@ -1,13 +1,60 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author Mert Bilgic
- */
-public class MainServer {
-    
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class MainServer extends Server {
+
+    private int controlTime;
+    private final int minCount = 0;
+    static int Balence = 0, Reponse = 0;
+
+    public MainServer(String serverName, int capacity, int requestTime, int responseTime, int serverIP,
+            boolean sleep, int controlTime, int maxRequestCount) {
+        super(serverName, capacity, requestTime, responseTime, serverIP, sleep, maxRequestCount);
+        this.controlTime = controlTime;
+    }
+
+    public int getControlTime() {
+        return controlTime;
+    }
+
+    public void setControlTime(int controlTime) {
+        this.controlTime = controlTime;
+    }
+
+    //Listemize fake verilerimizden yararlanarak request ekliyoruz.
+    public void request() {
+
+        int value, requestCount;
+        String ip;
+        while (true) {
+            try {
+                Thread.sleep(getRequestTime());
+
+                requestCount = random.nextInt(getMaxRequestCount());
+                if (getRequestData().size() + requestCount <= getCapacity()) {
+                    add(requestCount);
+                } else {
+                    requestCount = getCapacity() - getRequestData().size();
+                    add(requestCount);
+
+                }
+            } catch (InterruptedException ex) {
+                Logger.getLogger(MainServer.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+
+    }
+
+    public void add(int requestCount) {
+        int value;
+        String ip;
+        for (int i = 0; i < requestCount; i++) {
+            value = random.nextInt(12);
+            ip = String.valueOf(value);
+
+            requestData.add(new RequestData(clientIP + ip, requestName[value]));
+
+        }
+    }
 }
